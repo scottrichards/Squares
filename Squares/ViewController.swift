87b54c7fd79d2bc1069755b8b75f54eef37b23aa
Scott Richards <scott.richards@glassdoor.com>
2022-02-22 16:04:26 +0100

    Initial Commit

---
+++ Squares/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Squares
+//
+//  Created by Scott Richards on 2/22/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
